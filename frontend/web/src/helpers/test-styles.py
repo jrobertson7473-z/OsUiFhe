3315adb3f8341324818033715c291cc33cb6c2f6
jrobertson7473-z <YoukerSaieva9214@outlook.com>
2025-12-05 01:12:40 +0800

    add test-styles.py, dynamic.go, package-lock.json

---
+++ frontend/web/src/helpers/test-styles.py
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\frontend\web\src\helpers\test-styles.py
+// Generated at: 2025-10-17T08:19:12.007Z
---
+++ shared-serializer/dynamic.go
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\shared-serializer\dynamic.go
+// Generated at: 2025-10-17T08:19:11.990Z
